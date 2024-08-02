21c2ffec4e2c56f4670b48252dc12068263f3570
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-08-02 19:29:37 +0200

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import NodeGraph from "./components/NodeGraph";
-import data from "./data/Tutorial-nodes-3.json";
+import data from "./data/Test-nodes-53.json";
 import { GeometryNodeFile } from "./types";
 import '@xyflow/react/dist/style.css';
 
@@ -9,3 +9,4 @@ function App() {
 }
 
 export default App;
+
