f61a50b55b0ec650ca327a774c0d441a82b673cc
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-08-02 18:54:57 +0200

    add nodes ideas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { ReactFlow } from '@xyflow/react';
- 
+import NodeGraph from "./components/NodeGraph";
+import data from "./data/Tutorial-nodes-3.json";
+import { GeometryNodeFile } from "./types";
 import '@xyflow/react/dist/style.css';
- 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
-export default function App() {
-  return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
-    </div>
-  );
+
+
+function App() {
+  return <NodeGraph data={data as GeometryNodeFile} />;
 }
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import NodeGraph from "./components/NodeGraph"
+
+export {
+    NodeGraph
+}
