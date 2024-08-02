219aad7ac32379f5baeb4930e8f26d08127b318a
Jan-Hendrik Müller <44469195+kolibril13@users.noreply.github.com>
2024-08-02 19:43:57 +0200

    add ace editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
+import React from "react";
 import NodeGraph from "./components/NodeGraph";
 import data from "./data/Test-nodes-53.json";
 import { GeometryNodeFile } from "./types";
-import '@xyflow/react/dist/style.css';
-
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/theme-monokai";
+import "@xyflow/react/dist/style.css";
+import "./App.css";
 
 function App() {
-  return <NodeGraph data={data as GeometryNodeFile} />;
+  const [code, setCode] = React.useState(JSON.stringify(data, null, 2));
+  const [parsedData, setParsedData] = React.useState(data as GeometryNodeFile);
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+    try {
+      const updatedData = JSON.parse(newCode);
+      setParsedData(updatedData);
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+    }
+  };
+
+  return (
+    <div className="split-screen">
+      <div className="left-pane">
+        <AceEditor
+          mode="json"
+          theme="monokai"
+          value={code}
+          onChange={handleCodeChange}
+          name="codeEditor"
+          editorProps={{ $blockScrolling: true }}
+          setOptions={{ useWorker: false }}
+          width="100%"
+          height="100%"
+        />
+      </div>
+      <div className="right-pane">
+        <NodeGraph data={parsedData} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
-
